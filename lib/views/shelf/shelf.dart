0c90fb3e8c88c07daa297d126122a48320992135
leimenghao <leimenghao@huami.com>
2021-04-09 19:31:12 +0800

    feat |> 添加书架页面

---
+++ lib/views/shelf/shelf.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ShelfView extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ShelfViewState();
+  }
+}
+
+class _ShelfViewState extends State<ShelfView> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
