299a15650211e435c31f0080195e5bef479c33ac
lemon <leimenghao1991@gmail.com>
2021-04-11 18:58:23 +0800

    增加书架视图

---
+++ lib/views/shelf/bookinfo.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+
+import '../../bean/book.dart';
+
+class BookCoverWidget extends StatelessWidget {
+  final Book book;
+
+  const BookCoverWidget({Key key, this.book}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQuery.of(context).size.width;
+    return Expanded(
+      child: Stack(
+        children: [
+          Container(
+            color: Colors.grey,
+            margin: EdgeInsets.only(bottom: 15),
+          ),
+          Align(
+            child: Container(
+              child: Text(
+                book.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+                maxLines: 2,
+              ),
+              color: Colors.red,
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.only(left: 10, right: 10),
+            ),
+            alignment: Alignment.topCenter,
+            //
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ReadProgressWidget extends StatelessWidget {
+  final num progress;
+
+  const ReadProgressWidget({Key key, this.progress = 0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: CustomPaint(
+            child: Container(),
+            painter: _ProgressPainter(progress: progress),
+          ),
+        ),
+        Container(
+          child: Text(
+            "$progress%",
+            style: TextStyle(fontSize: 12, color: Colors.grey),
+          ),
+          margin: EdgeInsets.only(left: 5),
+        )
+      ],
+    );
+  }
+}
+
+/// 10个点表示总进度，深色点表示进度
+/// 如下，* 表示深色点; .表示浅色点，下图表示进度 40%-50%
+/// * * * * . . . . . .
+/// [progress] 表示进度，100表示100%
+class _ProgressPainter extends CustomPainter {
+  double progress;
+  double indicateRadius;
+  Color positionColor;
+  Color durationColor;
+  final Paint _paint = Paint();
+
+  _ProgressPainter(
+      {this.progress = 0,
+      this.indicateRadius = 1,
+      this.positionColor = Colors.black,
+      this.durationColor = Colors.grey}) {
+    _paint
+      ..isAntiAlias = true
+      ..strokeWidth = 1.0
+      ..style = PaintingStyle.fill
+      ..color = Colors.green
+      ..invertColors = false;
+  }
+
+  int get position {
+    int result = 0;
+    if (progress < 10) {
+      result = 1;
+    } else {
+      result = progress ~/ 10;
+    }
+    return result;
+  }
+
+  double calculateSpace(num width) {
+    return (width - indicateRadius * 2 * 10) / 9;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    print("size is: [${size.width}, ${size.height}]");
+    int pos = position;
+    num indicateSpace = calculateSpace(size.width);
+
+    num yCenter = size.height / 2;
+
+    // 绘制进度
+    int drawIndex = 0;
+    while (drawIndex < 10) {
+      _paint.color = drawIndex < pos ? positionColor : durationColor;
+      num xCenter =
+          drawIndex * (2 * indicateRadius + indicateSpace) + indicateRadius;
+      canvas.drawCircle(Offset(xCenter, yCenter), indicateRadius, _paint);
+      drawIndex++;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class ShelfBookWidget extends StatelessWidget {
+  final Book book;
+  final ReadInfo readInfo;
+
+  ShelfBookWidget({this.book, this.readInfo});
+
+  num get progress {
+    return readInfo.readPosition / book.size;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        BookCoverWidget(
+          book: book,
+        ),
+        ReadProgressWidget(
+          progress: progress,
+        )
+      ],
+    );
+  }
+}
--- lib/views/shelf/shelf.dart
+++ lib/views/shelf/shelf.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_reader/views/shelf/bookinfo.dart';
+
+import '../../bean/book.dart';
+import '../../bean/book.dart';
 
 class ShelfView extends StatefulWidget {
   @override
@@ -7,10 +11,58 @@ class ShelfView extends StatefulWidget {
   }
 }
 
+int randomIndex = 0;
+
+Book createBook(String bookName) {
+  randomIndex++;
+  return Book(
+      bookId: "$randomIndex",
+      name: bookName,
+      size: 100000,
+      cover: "",
+      path: "",
+      encode: "",
+      addTime: 100);
+}
+
+ReadInfo createReadInfo(String bookId) {
+  randomIndex++;
+  return ReadInfo(
+      bookId: bookId,
+      lastReadTime: 0,
+      readPosition: randomIndex * 1000,
+      lastRead: false);
+}
+
 class _ShelfViewState extends State<ShelfView> {
+  num bookCoverMaxWidth = 200;
+
+  List<Book> books = [
+    createBook("语文"),
+    createBook("十年高考三年模拟"),
+    createBook("理综"),
+    createBook("超级长的书名超级长的书名超级长的书名超级长的书名超级长的书名超级长的书名"),
+  ];
+  ReadInfo info = createReadInfo("book1.bookId");
+
+  num getBookCoverSpace(num shelfWidth) {
+    return (shelfWidth - bookCoverMaxWidth * 3) / 4;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, childAspectRatio: 0.707),
+        itemCount: books.length,
+        itemBuilder: (context, index) {
+          return Container(
+            child: ShelfBookWidget(
+              book: books[index],
+              readInfo: info,
+            ),
+            margin: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          );
+        });
   }
 }
